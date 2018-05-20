9908033bb9dc42c1d323d59cace0b1826f13b2b6
robinrestrepo814 <robinrestrepo814@gmail.com>
2018-05-20 03:10:55 -0500

    addimage

--- client/js/main.js
+++ client/js/main.js
@@ -3,9 +3,11 @@ var game = new Phaser.Game(640, 360, Phaser.AUTO);
 var mainState = {
     preload: function() {
         // Se cargan las imagenes, audios, etc
+        game.load.image('Background', '/client/assets/ppp.gif');
     },
     create: function() {
         // Se crean las cosas
+        Sprite = game.add.tileSprite(0, 0, 640, 360, 'Background');
     },
     update: function() {
         // Se ejecuta infinitamente constantemente
