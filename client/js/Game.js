c3d8986ffa3a637859c332b2ea734477ffc53f08
robinrestrepo814 <robinrestrepo814@gmail.com>
2018-05-21 22:10:58 -0500

    movimientos actualizados

--- client/js/Game.js
+++ client/js/Game.js
@@ -5,8 +5,8 @@ Game.init = function(){
 };
 
 Game.preload = function() {
-    game.load.image('fondo', '/client/assets/img/background.png');    
-    game.load.spritesheet('sprite','/client/assets/sprites/robot.png', 80, 111); // this will be the sprite of the players
+    game.load.image('fondo', '/client/assets/img/2.jpg');    
+    game.load.spritesheet('sprite','/client/assets/sprites/robot_blue.png', 80, 111,35); // this will be the sprite of the players
 };
 
 Game.create = function(){
@@ -33,13 +33,26 @@ Game.movePlayer = function(id, x, y){
     var tween = game.add.tween(player);
     tween.to({x:x,y:y}, 0.1);
     tween.start();
+    //Animations of the robot
     if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)){
         player.animations.play('walk');
         player.scale.x=1;
     }else if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT)){
         player.animations.play('walk');
         player.scale.x=-1;
-    } else {
+    }else if (game.input.keyboard.isDown(Phaser.Keyboard.Q)){
+        player.animations.play('punch');
+        player.scale.x=-1;
+    }else if (game.input.keyboard.isDown(Phaser.Keyboard.W)){
+        player.animations.play('punch');
+        player.scale.x=1;
+    }else if (game.input.keyboard.isDown(Phaser.Keyboard.E)){
+        player.animations.play('block');
+        player.scale.x=-1;
+    }else if (game.input.keyboard.isDown(Phaser.Keyboard.R)){
+        player.animations.play('block');
+        player.scale.x=1;
+    }else {
         player.animations.play('idle');
     }
 };
@@ -48,7 +61,8 @@ Game.addNewPlayer = function(id,x,y){
     Game.playerMap[id] = game.add.sprite(x,y,'sprite');
     Game.playerMap[id].animations.add('walk', [10,11,12,13,14,15,16,17], 12,true, "sprite");
     Game.playerMap[id].animations.add('idle', [0,1,2,3,4,5,6,7,8,9], 12,true, "sprite");
-    Game.playerMap[id].animations.add('jump', [18,19,20,21,22,23,24,25], 12,false, "sprite");
+    Game.playerMap[id].animations.add('punch', [10,14,19], 25,false, "sprite");
+    Game.playerMap[id].animations.add('block', [24,25,25], 12,false);
 };
 
 Game.removePlayer = function(id){
